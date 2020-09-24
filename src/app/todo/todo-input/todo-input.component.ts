7ba15b3b03a85a6a4e1186c45b082361e693d8af
billdelvin <billdelvin6@gmail.com>
2020-09-24 21:07:02 +0700

    make initialForm and submit

--- src/app/todo/todo-input/todo-input.component.ts
+++ src/app/todo/todo-input/todo-input.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { TodoService } from "../todo-service.service";
 
 @Component({
-  selector: 'app-todo-input',
-  templateUrl: './todo-input.component.html',
-  styleUrls: ['./todo-input.component.css']
+  selector: "app-todo-input",
+  templateUrl: "./todo-input.component.html",
+  styleUrls: ["./todo-input.component.css"],
 })
 export class TodoInputComponent implements OnInit {
+  constructor(private todoService: TodoService) {}
 
-  constructor() { }
+  todoForm: FormGroup;
 
   ngOnInit(): void {
+    this.initialForm();
   }
 
+  onSubmit() {
+    this.todoService.addTodo(this.todoForm.value);
+  }
+
+  private initialForm() {
+    this.todoForm = new FormGroup({
+      day: new FormControl("", Validators.required),
+      activities: new FormControl("", Validators.required),
+    });
+  }
 }
