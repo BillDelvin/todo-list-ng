ce281f1d8121ba9e43077a47d8cfb2ddb042df48
billdelvin <billdelvin6@gmail.com>
2020-09-25 00:06:42 +0700

    adding configurtion for edit

--- src/app/todo/todo-input/todo-input.component.ts
+++ src/app/todo/todo-input/todo-input.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Subscription } from "rxjs";
 import { TodoService } from "../todo-service.service";
 import { Todos } from "../todo.model";
 
@@ -8,23 +10,54 @@ import { Todos } from "../todo.model";
   templateUrl: "./todo-input.component.html",
   styleUrls: ["./todo-input.component.css"],
 })
-export class TodoInputComponent implements OnInit {
-  constructor(private todoService: TodoService) {}
-
+export class TodoInputComponent implements OnInit, OnDestroy {
   todoForm: FormGroup;
+  idLength: number = 0;
+  paramsSubs: Subscription;
   id: number = 0;
+  editMode: boolean = false;
+  todo: Todos;
+  todos: Todos[];
 
-  ngOnInit(): void {
+  constructor(
+    private todoService: TodoService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.id = +this.route.snapshot.paramMap.get("id");
     this.initialForm();
   }
 
+  ngOnInit() {
+    this.todos = this.todoService.getTodos();
+
+    if (this.id != 0) {
+      this.editMode = true;
+      this.todo = this.todoService.getTodo(this.id);
+      if (!this.todo) {
+        this.router.navigate(["/"]);
+      } else {
+        this.todoForm = new FormGroup({
+          day: new FormControl(this.todo.day, Validators.required),
+          activities: new FormControl(
+            this.todo.activities,
+            Validators.required
+          ),
+        });
+      }
+    }
+  }
+
   onSubmit() {
-    const todo: Todos = {
-      id: this.id++,
-      day: this.todoForm.value.day,
-      activities: this.todoForm.value.activities,
-    };
-    this.todoService.addTodo(todo);
+    if (this.editMode) {
+    } else {
+      const todo: Todos = {
+        id: this.todos.length + 1,
+        day: this.todoForm.value.day,
+        activities: this.todoForm.value.activities,
+      };
+      this.todoService.addTodo(todo);
+    }
     this.initialForm();
   }
 
@@ -33,5 +66,9 @@ export class TodoInputComponent implements OnInit {
       day: new FormControl("", Validators.required),
       activities: new FormControl("", Validators.required),
     });
+  }
+
+  ngOnDestroy() {
+    // this.paramsSubs.unsubscribe();
   }
 }
